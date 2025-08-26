696d8dcead2e412bdb096c07866ce07e772e9278
Yhwach <daniel.fernandes.santos19@escola.pr.gov.br>
2025-08-26 15:09:05 +0000

    ez

---
+++ scipt.js
@@ -0,0 +1,12 @@
+const calcular = document.getElementById('calcular');
+
+function imc(){
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado').value;
+    
+    const valorIMC = (peso/(altura*altura))_.toFixed;
+
+    resultado.textcontent = `${nome}seu IMC é ${valorIMC}!`
+}
